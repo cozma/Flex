7159dd3f1e98a9143e8521bc62be00807baafb08
Oliver Ebeling-Koning <odek@vt.edu>
2016-02-27 03:25:50 -0500

    Jk here is the working version

--- database/apiRequesting.py
+++ database/apiRequesting.py
@@ -1,41 +1,64 @@
-# This is written for PYTHON 3
-# Don't forget to install requests package
+# This creates charges and deposits on someone's account
+# NOTE: that it takes time for the balances to update!!
 
 import requests
 import json
 
-customerId = '56c66be5a73e4927415073da'
-apiKey = '52da742eb132c5000831254a4002207a'
+customerId = '56c66be5a73e4927415073f1'
+apiKey = 'f2074f6de40823eda9cf1a33f0d23279'
 
 
-accountsurl = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId, apiKey)
-
-response = requests.get(accountsurl)
-
-print response.status_code()
-
-card_list = response.json()
-
-for account in card_list:
-    if(account['type'] != 'Credit Card' and account['type'] != 'Checking'):
-        card_list.remove(account)
-
+def depositMoney(accountID, amountToPutIn):
+    depositurl = 'http://api.reimaginebanking.com/accounts/' + accountID + '/deposits?key=' + apiKey
+    deposit = {"medium": "balance","transaction_date": "2016-02-27","status": "pending","amount": amountToPutIn,"description": "string"}
+    response = requests.post(
+	    depositurl,
+	    data=json.dumps(deposit),
+	    headers={'content-type':'application/json', 'Accept': 'application/json'},
+	)
+    print(response)
+    print response.json()
 
 
-for account in card_list:
-    print account['_id']
-    purchaseurl = 'http://api.reimaginebanking.com/accounts/{}/purchases?key={}'.format(account['_id'], apiKey)
-
+def chargeToAccount(accountID, amountToCharge):
+    purchaseurl = 'http://api.reimaginebanking.com/accounts/' + accountID + '/purchases?key=' + apiKey
+    purchase = {"merchant_id": "56c66be6a73e492741507627","medium": "balance","purchase_date": "2016-02-27","amount": amountToCharge,"status": "pending","description": "string"}
     response = requests.post(
 	    purchaseurl,
 	    data=json.dumps(purchase),
-	    headers={'content-type':'application/json'},
+	    headers={'content-type':'application/json', 'Accept': 'application/json'},
 	)
     print(response)
+    print response.json()
+
+#################################################################################################
+#################################################################################################
+#################################################################################################
+
+accountsurl = 'http://api.reimaginebanking.com/customers/' + customerId + '/accounts?key=' + apiKey
+
+response = requests.get(accountsurl)
+
+card_list = response.json()
+print card_list # print all the accounts
+
+accountNum = card_list[0]['_id'] # Get the id number for the first account
 
 
+###############################
+##### Main deposits and charges
+chargeToAccount(accountNum, 200)
+depositMoney(accountNum, 5000)
+   
 
 
+print
+print
+for account in card_list:
+    getPurchases = 'http://api.reimaginebanking.com/accounts/' + account['_id'] + '/purchases?key=' + apiKey
+
+    response = requests.get(accountsurl)
+    print response.json()[0]['nickname'] + " currently has " + str(response.json()[0]['balance']) + " dollars"
 
 
 
