df0a92e142b8eb1a0450a356532297ad8500fa24
John Purviance <ffdd4846@vt.edu>
2016-02-27 11:34:07 -0500

    we have auth

--- index.py
+++ index.py
@@ -1,10 +1,20 @@
 from flask import Flask, send_from_directory, request, redirect, url_for
+import sys
 app = Flask(__name__, static_url_path='')
 
 @app.route('/')
 def index():
     return send_from_directory("static", "index.html")
 
+
+@app.route('/active', methods=['POST'])
+def login():
+	password = str(request.form['password'])
+	username = str(request.form['email'])
+	if (username == "test@email.com" and password == "test"):
+		return send_from_directory("static", "stat.html")
+	else:
+		return send_from_directory("static", "index.html")
 
 @app.route('/<path:path>')
 def static_proxy(path):
